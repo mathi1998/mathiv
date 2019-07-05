4c13fa1f3d241a011fd1ef1dfc97e4024c718f63
training_d2.03.07 <training_d2.03.07@ddc2-d-9r84t72.dir.svc.accenture.com>
2019-07-05 14:57:37 +0530

    commit

---
+++ src/test/java/jjj/case2.java
@@ -0,0 +1,17 @@
+package jjj;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions
+(
+		plugin= {"json:cucumber.jason"}
+		)
+
+public class case2 {
+
+}
+
